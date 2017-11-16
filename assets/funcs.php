bf479f51369d762f8722f69be5b6d0e61fed467b
voltainc <voltaincx@gmail.com>
2017-11-16 21:29:36 +0400

    intial commit

--- assets/funcs.php
+++ assets/funcs.php
@@ -258,7 +258,7 @@
 										<head>
 										  <meta charset='utf-8'>
 										  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-										  <title>SSR | RES</title>
+										  <title>Artist Booking System</title>
 										  <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 										  <link rel='stylesheet' href='{$arr[2]}font-awesome/css/font-awesome.min.css'>
 										  <link rel='stylesheet' href='{$arr[2]}bootstrap/css/bootstrap.min.css'>
